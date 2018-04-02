fb46ae4a370606858565a9b18678d476579582e6
Juan Cabello <jcabelloc@gmail.com>
2018-04-01 22:48:57 -0500

    Show Properties v2

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ var properties = [
         url: "http://roitoledo.com/public/uploads/sites/2017_11_28_02_03_40_1__3property-sale-2.jpg",
         address: "4051 Southern Trace Dr, College Station, TX 77845",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut ante leo. Vestibulum ac ullamcorper urna. Duis tempus auctor risus, sed viverra sapien gravida et. Cras hendrerit iaculis sapien, ac volutpat purus egestas gravida"
+    },
+    {
+        price: "550",
+        url: "https://myupdatestudio.com/wp-content/uploads/2018/01/clubhousewelcome2i-min.jpg",
+        address: "4051 Southern Trace Dr, College Station, TX 77845",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut ante leo. Vestibulum ac ullamcorper urna. Duis tempus auctor risus, sed viverra sapien gravida et. Cras hendrerit iaculis sapien, ac volutpat purus egestas gravida"
     }
 ];
 
